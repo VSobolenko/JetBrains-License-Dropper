24f7c0a4b17973689b5654cab8fec034172503a0
VladislavS <v.sobolenko.vs@gmail.com>
2022-10-22 01:09:29 +0300

    bug fixes, use console color and code clean up

--- Eval/JetBrainsLicenseReset.cs
+++ Eval/JetBrainsLicenseReset.cs
@@ -1,5 +1,4 @@
-﻿
-#define ENABLE_LOG
+﻿#define ENABLE_LOG
 #define ENABLE_DELETE_ACTION
 
 using System;
@@ -10,180 +9,184 @@ using Microsoft.Win32;
 
 namespace Eval
 {
-    internal enum RegistryKeyType
-    {
-        CurrentUser,
-        Users,
-    }
-    
-    internal class JetBrainsLicenseReset
-    {
-        private readonly List<string> _tempFolders = new List<string>
-        {
-            Path.GetTempPath(),
-        };
-        
-        private readonly List<Tuple<string, string>> _folderPosition = new List<Tuple<string, string>>
-        {
-            new Tuple<string, string>(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "JetBrains"),
-            new Tuple<string, string>(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "JetBrains"),
-        };
+internal enum RegistryKeyType
+{
+    CurrentUser,
+    Users,
+}
 
-        private readonly List<Tuple<RegistryKeyType, string>> _registryPosition = new List<Tuple<RegistryKeyType, string>>
-        {
-            new Tuple<RegistryKeyType, string>(RegistryKeyType.CurrentUser, @"SOFTWARE\JetBrains"),
-            new Tuple<RegistryKeyType, string>(RegistryKeyType.Users, @"S-1-5-21-3093079274-4027988934-4014703911-1001\SOFTWARE\JavaSoft\Prefs\jetbrains"),
-        };
-        
-        private readonly List<Tuple<RegistryKeyType, string>> _registrySpecialJavaPosition = new List<Tuple<RegistryKeyType, string>>
+internal class JetBrainsLicenseReset
+{
+    private readonly List<Tuple<string, string>> _folderPosition = new List<Tuple<string, string>>
+    {
+        new Tuple<string, string>(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "JetBrains"),
+        new Tuple<string, string>(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                                  "JetBrains"),
+    };
+
+    private readonly Action<string> _log;
+
+    private readonly List<Tuple<RegistryKeyType, string>> _registryPosition = new List<Tuple<RegistryKeyType, string>>
+    {
+        new Tuple<RegistryKeyType, string>(RegistryKeyType.CurrentUser, @"SOFTWARE\JetBrains"),
+        new Tuple<RegistryKeyType, string>(RegistryKeyType.Users,
+                                           @"S-1-5-21-3093079274-4027988934-4014703911-1001\SOFTWARE\JavaSoft\Prefs\jetbrains"),
+    };
+
+    private readonly List<Tuple<RegistryKeyType, string>> _registrySpecialJavaPosition =
+        new List<Tuple<RegistryKeyType, string>>
         {
             new Tuple<RegistryKeyType, string>(RegistryKeyType.CurrentUser, @"SOFTWARE\JavaSoft\Prefs\jetbrains"),
-            new Tuple<RegistryKeyType, string>(RegistryKeyType.Users, @"S-1-5-21-3093079274-4027988934-4014703911-1001\SOFTWARE\JavaSoft\Prefs\jetbrains"),
+            new Tuple<RegistryKeyType, string>(RegistryKeyType.Users,
+                                               @"S-1-5-21-3093079274-4027988934-4014703911-1001\SOFTWARE\JavaSoft\Prefs\jetbrains"),
         };
 
-        private readonly Action<string> _log;
+    private readonly List<string> _tempFolders = new List<string>
+    {
+        Path.GetTempPath(),
+    };
 
-        public JetBrainsLicenseReset(Action<string> log) => _log = log;
-        
-        #region Folder
+    public JetBrainsLicenseReset(Action<string> log)
+    {
+        _log = log;
+    }
 
-        public bool ClearFolderMemory() => ClearFolderMemory(_folderPosition);
-
-        private bool ClearFolderMemory(IEnumerable<Tuple<string, string>> positions)
-        {
-            var allPathsDelete = true;
-            foreach (var (pathToFolder, folderName) in positions)
-            {
-                try
-                {
-                    allPathsDelete = DeleteFolder(pathToFolder, folderName) && allPathsDelete;
-                }
-                catch (Exception e)
-                {
-                    Log($"[Folder][Error] Delete key:{pathToFolder} in path:{folderName} with exception: {e.Message}");
-                    allPathsDelete = false;
-                }
-            }
-
-            return allPathsDelete;
-        }
-
-        public bool ClearTempFiles() => ClearFolderInsides(_tempFolders);
-        
-        private bool ClearFolderInsides(IEnumerable<string> positions)
-        {
-            foreach (var position in positions)
-            {
-                var directoryInfo = new DirectoryInfo(position);
-
-                foreach (FileInfo file in directoryInfo.GetFiles())
-                {
-                    try
-                    {
-                        var filePath = Path.Combine(file.Name);
-#if ENABLE_DELETE_ACTION
-                        file.Delete(); 
-#endif
-                        Log($"[Folder][Success] Delete file: {filePath}");
-                    }
-                    catch (Exception e)
-                    {
-                        Log($"[Folder][Error] Delete file: {e.Message}");
-                    } 
-                }
-                foreach (DirectoryInfo directory in directoryInfo.GetDirectories())
-                {
-                    try
-                    {
-                        var directoryPath = directory.FullName;
-#if ENABLE_DELETE_ACTION
-                        directory.Delete(true);
-#endif
-                        Log($"[Folder][Success] Delete file: {directoryPath}");
-                    }
-                    catch (Exception e)
-                    {
-                        Log($"[Folder][Error] Delete error: {e.Message}");
-                    }
-                }
-            }
-            return true;
-        }
-        
-        private bool DeleteFolder(string pathToFOlder, string folder)
-        {
-            var path = Path.Combine(pathToFOlder, folder);
-            if (Directory.Exists(path) == false) 
-                return false;
-            Log($"[Folder][Success] Delete key path:{path}");
-            
-#if ENABLE_DELETE_ACTION
-            Directory.Delete(path);
-#endif
-            return true;
-        }
-
-        #endregion
-
-        #region Registry
-
-        public bool ClearRegistryMemory() => ClearRegistryMemory(_registryPosition);
-        
-        public bool ClearSpecialJavaMemory() => ClearRegistryMemory(_registrySpecialJavaPosition);
-
-        private bool ClearRegistryMemory(List<Tuple<RegistryKeyType, string>> positions)
-        {
-            var allPathsDelete = true;
-            foreach (var position in positions)
-            {
-                try
-                {
-                    DeleteRegistryKey(position.Item1, position.Item2);
-                }
-                catch (Exception e)
-                {
-                    Log($"[Registry][Error] Delete key:{position.Item1} in path:{position.Item2} with exception: {e.Message}");
-                    allPathsDelete = false;
-                }
-            }
-
-            return allPathsDelete;
-        }
-        
-        private void DeleteRegistryKey(RegistryKeyType registryKeyType, string path)
-        {
-            using (var key = GetRegistryKeyByType(registryKeyType))
-            {
-                Log($"[Registry][Success] Delete key path:{path}");
-                
-#if ENABLE_DELETE_ACTION
-                key.DeleteSubKeyTree(path);
-#endif
-            }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private RegistryKey GetRegistryKeyByType(RegistryKeyType registryKeyType)
-        {
-            switch (registryKeyType)
-            {
-                case RegistryKeyType.CurrentUser:
-                    return Registry.CurrentUser;
-                case RegistryKeyType.Users:
-                    return Registry.Users;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(registryKeyType), registryKeyType, "Unsupported registry key Used");
-            }
-        }
-
-        #endregion
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void Log(string message)
-        {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Log(string message)
+    {
 #if ENABLE_LOG
-            _log?.Invoke(message);
+        _log?.Invoke(message);
+#endif
+    }
+
+    #region Folder
+
+    public bool ClearFolderMemory() => ClearFolderMemory(_folderPosition);
+
+    private bool ClearFolderMemory(IEnumerable<Tuple<string, string>> positions)
+    {
+        var allPathsDelete = true;
+        foreach (var (pathToFolder, folderName) in positions)
+            try
+            {
+                allPathsDelete = DeleteFolder(pathToFolder, folderName) && allPathsDelete;
+            }
+            catch (Exception e)
+            {
+                Log($"[Folder][Error] Delete key:{pathToFolder} in path:{folderName} with exception: {e.Message}");
+                allPathsDelete = false;
+            }
+
+        return allPathsDelete;
+    }
+
+    public bool ClearTempFiles() => ClearFolderInsides(_tempFolders);
+
+    private bool ClearFolderInsides(IEnumerable<string> positions)
+    {
+        foreach (var position in positions)
+        {
+            var directoryInfo = new DirectoryInfo(position);
+
+            foreach (var file in directoryInfo.GetFiles())
+                try
+                {
+                    var filePath = Path.Combine(file.Name);
+#if ENABLE_DELETE_ACTION
+                    file.Delete();
+#endif
+                    Log($"[Folder][Success] Delete file: {filePath}");
+                }
+                catch (Exception e)
+                {
+                    Log($"[Folder][Error] Delete file: {e.Message}");
+                }
+
+            foreach (var directory in directoryInfo.GetDirectories())
+                try
+                {
+                    var directoryPath = directory.FullName;
+#if ENABLE_DELETE_ACTION
+                    directory.Delete(true);
+#endif
+                    Log($"[Folder][Success] Delete file: {directoryPath}");
+                }
+                catch (Exception e)
+                {
+                    Log($"[Folder][Error] Delete error: {e.Message}");
+                }
+        }
+
+        return true;
+    }
+
+    private bool DeleteFolder(string pathToFOlder, string folder)
+    {
+        var path = Path.Combine(pathToFOlder, folder);
+
+        if (Directory.Exists(path) == false)
+            return false;
+
+#if ENABLE_DELETE_ACTION
+        Directory.Delete(path, true);
+#endif
+        Log($"[Folder][Success] Delete key path:{path}");
+        return true;
+    }
+
+    #endregion
+
+    #region Registry
+
+    public bool ClearRegistryMemory() => ClearRegistryMemory(_registryPosition);
+
+    public bool ClearSpecialJavaMemory() => ClearRegistryMemory(_registrySpecialJavaPosition);
+
+    private bool ClearRegistryMemory(List<Tuple<RegistryKeyType, string>> positions)
+    {
+        var allPathsDelete = true;
+        foreach (var position in positions)
+            try
+            {
+                DeleteRegistryKey(position.Item1, position.Item2);
+            }
+            catch (Exception e)
+            {
+                Log(
+                    $"[Registry][Error] Delete key:{position.Item1} in path:{position.Item2} with exception: {e.Message}");
+                allPathsDelete = false;
+            }
+
+        return allPathsDelete;
+    }
+
+    private void DeleteRegistryKey(RegistryKeyType registryKeyType, string path)
+    {
+        using (var key = GetRegistryKeyByType(registryKeyType))
+        {
+            Log($"[Registry][Success] Delete key path:{path}");
+
+#if ENABLE_DELETE_ACTION
+            key.DeleteSubKeyTree(path);
 #endif
         }
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private RegistryKey GetRegistryKeyByType(RegistryKeyType registryKeyType)
+    {
+        switch (registryKeyType)
+        {
+            case RegistryKeyType.CurrentUser:
+                return Registry.CurrentUser;
+            case RegistryKeyType.Users:
+                return Registry.Users;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(registryKeyType), registryKeyType,
+                                                      "Unsupported registry key Used");
+        }
+    }
+
+    #endregion
+}
 }
--- Eval/Program.cs
+++ Eval/Program.cs
@@ -2,63 +2,88 @@
 
 namespace Eval
 {
-    internal static class Program
+internal static class Program
+{
+    public static void Main()
     {
-        public static void Main()
+        Action<string> log = Console.WriteLine;
+
+        if (IsRunResetLicense())
         {
-            Action<string> log = Console.WriteLine;
-            
-            if (IsRunResetLicense())
+            try
             {
-                try
-                {
-                    var jetbrainsData = new JetBrainsLicenseReset(log);
+                var jetbrainsData = new JetBrainsLicenseReset(log);
 
-                    if (IsClearAllTempFiles())
-                        jetbrainsData.ClearTempFiles();
-                    if (IsClearSpecialJavaMemory())
-                        jetbrainsData.ClearSpecialJavaMemory();
-                
-                    jetbrainsData.ClearFolderMemory();
-                    jetbrainsData.ClearRegistryMemory();
+                InvokeResetMethod(IsClearAllTempFiles, () => jetbrainsData.ClearTempFiles(),
+                    "Cleaning up temporary files", log);
+                InvokeResetMethod(IsClearSpecialJavaMemory, () => jetbrainsData.ClearSpecialJavaMemory(),
+                    "Clearing Java Special Data", log);
+                InvokeResetMethod(() => true, () => jetbrainsData.ClearFolderMemory(),
+                    "Clearing folders", log);
+                InvokeResetMethod(() => true, () => jetbrainsData.ClearRegistryMemory(),
+                    "Clearing registry", log);
 
-                    log.Invoke("License reset!");
-                }
-                catch (Exception e)
-                {
-                    log.Invoke($"Error reset eval with exception: {e.Message}");
-                }   
+                log?.Invoke("License reset complete!");
             }
-            else
+            catch (Exception e)
             {
-                log.Invoke("Reset skipped");
+                log?.Invoke($"Error reset eval with exception: {e.Message}");
             }
+        }
+        else
+            log?.Invoke("Reset skipped");
 
-            Console.ReadKey();
-        }
-
-        private static bool IsClearSpecialJavaMemory()
-        {
-            Console.WriteLine("(yes/no) Clean up special folders from java? (if it didn't work without it)");
-            return IsUserAgreed();
-        }
-        
-        private static bool IsRunResetLicense()
-        {
-            Console.WriteLine("(yes/no) Reset license?");
-            return IsUserAgreed();
-        }
-        
-        private static bool IsClearAllTempFiles()
-        {
-            Console.WriteLine("(yes/no) Clear all temp files? (desirable)?");
-            return IsUserAgreed();
-        }
-
-        private static bool IsUserAgreed()
-        {
-            var userInput = Console.ReadLine();
-            return userInput == "yes";
-        }
+        Console.ReadKey();
     }
+
+    private static void InvokeResetMethod(Func<bool> condition, Action action, string description, Action<string> log)
+    {
+        if (condition.Invoke() == false)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            
+            log?.Invoke($"[Skip] {description}");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return;
+        }
+        Console.ForegroundColor = ConsoleColor.Green;
+        log?.Invoke($"[Start] {description}");
+
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        action?.Invoke();
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        log?.Invoke($"[End] {description}\n");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static bool IsClearSpecialJavaMemory()
+    {
+        Console.WriteLine("(yes/no) Clean up special folders from java? (if it didn't work without it)");
+
+        return IsUserAgreed();
+    }
+
+    private static bool IsRunResetLicense()
+    {
+        Console.WriteLine("(yes/no) Reset license?\n");
+
+        return IsUserAgreed();
+    }
+
+    private static bool IsClearAllTempFiles()
+    {
+        Console.WriteLine("(yes/no) Clear all temp files? (desirable)?");
+
+        return IsUserAgreed();
+    }
+
+    private static bool IsUserAgreed()
+    {
+        var userInput = Console.ReadLine();
+
+        return userInput == "yes";
+    }
+}
 }
